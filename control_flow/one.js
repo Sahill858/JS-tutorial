c358ad70e019dbe022922e4e343ab79f51afba1d
Sahil <sahil.sahu@cloudbrx.in>
2025-10-16 16:10:04 +0530

    Control Flow

---
+++ control_flow/one.js
@@ -0,0 +1,51 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+
+
+// if (temperature === 41){
+//     console.log("less than 50")
+// }
+// else{
+// console.log("temperature is greature than 50")
+// }
+
+// console.log("Excecuted")
+
+// const score = 200;
+
+// if (score > 100) {
+//   let power = "fly";
+//   console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance  = 750
+
+// if (balance < 500){
+//     console.log("less than 500");
+// } else if (balance<750){
+//     console.log("less than 700")
+// }
+//  else if (balance<900){
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course")
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in")
+}
